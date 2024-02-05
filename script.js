893a49dbf7c80cbd4fe174e544ebd8814e8f6183
sebastianmsz <sevas.molina2004@gmail.com>
2024-02-04 22:03:24 -0500

    Implement add book menu
    Display menu to add a book when clicked the 'Add book' button.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 document.querySelector("#year").textContent = new Date().getFullYear();
+
+(function () {
+    const addBookBtn = document.querySelector("#addBookBtn");
+    const cancelButton = document.querySelector("#cancel");
+    const dialog = document.querySelector("#dialog");
+
+    addBookBtn.addEventListener("click", function () {
+      dialog.showModal();
+    });
+
+    cancelButton.addEventListener("click", function () {
+      dialog.close();
+    });
+})()
